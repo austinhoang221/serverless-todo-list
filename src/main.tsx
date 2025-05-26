448031d025cf0e226d0bf7b1bae0d26db8dd0768
austinhoang221 <hbinhit221@gmail.com>
2025-05-26 23:50:59 +0700

    Config Cognito SDK and Amplify UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Theme, ThemeProvider } from "@aws-amplify/ui-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+const theme: Theme = {
+  name: 'my-theme',
+  tokens: {
+    colors: {
+      font: {
+        primary: { value: '#008080' },
+        // ...
+      },
+    },
+  },
+};
+// wrap the application with AuthProvider
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+      <ThemeProvider theme={theme}>
+      <App />
+      </ThemeProvider>
+  </React.StrictMode>
+);
