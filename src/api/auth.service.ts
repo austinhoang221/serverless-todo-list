b0f934dda2e46bb0caf965ae5d8d3123f1ca3f66
austinhoang221 <hbinhit221@gmail.com>
2025-06-06 16:59:01 +0700

    Add auth context

--- src/api/auth.service.ts
+++ src/api/auth.service.ts
@@ -1,81 +1,100 @@
 import {
-    CognitoIdentityProviderClient,
-    ConfirmSignUpCommand,
-    InitiateAuthCommand,
-    SignUpCommand,
-  } from "@aws-sdk/client-cognito-identity-provider";
+  AdminGetUserCommand,
+  CognitoIdentityProviderClient,
+  ConfirmSignUpCommand,
+  InitiateAuthCommand,
+  SignUpCommand,
+} from "@aws-sdk/client-cognito-identity-provider";
 
-  const REGION = "us-east-1"; // replace with your region
-  const CLIENT_ID = "29fbdv3ovcfrhcjldcn7p12car"; // replace with your Cognito App Client ID
-  
-  const cognitoClient = new CognitoIdentityProviderClient({ region: REGION });
-  
-  export const signIn = async (username: string, password: string) => {
-    const command = new InitiateAuthCommand({
-      AuthFlow: "USER_PASSWORD_AUTH",
-      ClientId: CLIENT_ID,
-      AuthParameters: {
-        USERNAME: username,
-        PASSWORD: password,
-      },
-    });
-  
-    try {
-      const response = await cognitoClient.send(command);
-      console.log("Auth successful", response);
-  
-      if (response.AuthenticationResult) {
-        const { AccessToken, IdToken, RefreshToken } = response.AuthenticationResult;
-        // You can now store these tokens (e.g., localStorage) or use them as needed
-        return { AccessToken, IdToken, RefreshToken };
-      }
-  
-      // Handle if a challenge like NEW_PASSWORD_REQUIRED comes up
-      if (response.ChallengeName) {
-        return { challenge: response.ChallengeName, session: response.Session };
-      }
-    } catch (error) {
-      console.error("Auth failed", error);
-      throw error;
-    }
-  };
-  
+const REGION = process.env.REACT_APP_REGION; // replace with your region
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID; // replace with your Cognito App Client ID
 
-  export const signUp = async (email: string, nickName: string) => {
-    const command = new SignUpCommand({
-      ClientId: CLIENT_ID, 
-  Username: email, 
-  UserAttributes: [
-    {
-      Name: "nickname", 
-      Value: nickName,
+const cognitoClient = new CognitoIdentityProviderClient({ region: REGION });
+
+export const signIn = async (username: string, password: string) => {
+  const command = new InitiateAuthCommand({
+    AuthFlow: "USER_PASSWORD_AUTH",
+    ClientId: CLIENT_ID,
+    AuthParameters: {
+      USERNAME: username,
+      PASSWORD: password,
     },
-  ],
-    });
-    try {
-      const response = await cognitoClient.send(command);
-      console.log("SignUp success:", response);
-      return response;
-    } catch (error) {
-      console.error("SignUp error:", error);
-      throw error;
-    }
-  };
-  
+  });
 
-  export const confirmSignUpUser = async (email: string, code: string) => {
-    const command = new ConfirmSignUpCommand({
-      ClientId: CLIENT_ID,
-      Username: email,
-      ConfirmationCode: code,
-    });
-  
-    try {
-      const response = await cognitoClient.send(command);
-      console.log("Confirmation success:", response);
-      return response;
-    } catch (error) {
-      console.error("Confirmation error:", error);
-      throw error;
+  try {
+    const response = await cognitoClient.send(command);
+    console.log("Auth successful", response);
+    if (response.AuthenticationResult) {
+      const { AccessToken, IdToken, RefreshToken } =
+        response.AuthenticationResult;
+      // You can now store these tokens (e.g., localStorage) or use them as needed
+      const command = new AdminGetUserCommand({
+        UserPoolId: IdToken,
+        Username: username,
+      });
+
+      if (IdToken) {
+        const getUserResponse = await cognitoClient.send(command);
+        const { Username, UserCreateDate, UserLastModifiedDate, UserStatus } =
+          getUserResponse;
+        return {
+          accessToken: AccessToken,
+          userId: IdToken,
+          userName: Username,
+          refreshToken: RefreshToken,
+          lastModifiedDate: UserLastModifiedDate,
+          createdDate: UserCreateDate,
+          status: UserStatus,
+        };
+      }
     }
-  };
+
+    // Handle if a challenge like NEW_PASSWORD_REQUIRED comes up
+    if (response.ChallengeName) {
+      return { challenge: response.ChallengeName, session: response.Session };
+    }
+  } catch (error) {
+    console.error("Auth failed", error);
+    throw error;
+  }
+};
+
+export const signUp = async (email: string, nickName: string) => {
+  const command = new SignUpCommand({
+    ClientId: CLIENT_ID,
+    Username: email,
+    UserAttributes: [
+      {
+        Name: "nickname",
+        Value: nickName,
+      },
+    ],
+  });
+  try {
+    const response = await cognitoClient.send(command);
+
+    console.log("SignUp success:", response);
+    return response;
+  } catch (error) {
+    console.error("SignUp error:", error);
+    throw error;
+  }
+};
+
+export const confirmSignUpUser = async (email: string, code: string) => {
+  const command = new ConfirmSignUpCommand({
+    ClientId: CLIENT_ID,
+    Username: email,
+    ConfirmationCode: code,
+  });
+
+  try {
+    const response = await cognitoClient.send(command);
+    console.log("Confirmation success:", response);
+
+    return response;
+  } catch (error) {
+    console.error("Confirmation error:", error);
+    throw error;
+  }
+};
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,27 +1,13 @@
-import { createContext, useContext, useState } from "react";
+import { AdminGetUserCommandOutput } from "@aws-sdk/client-cognito-identity-provider";
+import { createContext } from "react";
 
 interface AuthContextType {
-    isAuthenticated: boolean;
-    changeAuthenticateState: (state: boolean) => void
-  }
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
-
-export const AuthenticatedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) =>{
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-    const changeAuthenticateState = (state: boolean) => {
-        setIsAuthenticated(state);
-    }
-    return (
-        <AuthContext.Provider value={{ isAuthenticated, changeAuthenticateState}}>
-          {children}
-        </AuthContext.Provider>
-      );
+  loginResult: string;
+  refreshToken: string;
+  token: string;
+  userName: string;
+  user: AdminGetUserCommandOutput;
 }
-
-
-export const useAuth = () => {
-    const context = useContext(AuthContext);
-    if (!context) throw new Error("useAuth must be used within an AuthProvider");
-    return context;
-  };
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
